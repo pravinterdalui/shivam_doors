390df8ab8a7de34803507178919b5883faa3a7f7
Pravin Terdal <your-github-email@example.com>
2026-09-13 05:05:39 +0530

    updated design

---
+++ src/components/register/register.jsx
@@ -0,0 +1,13 @@
+
+
+export function RegisterDemo(){
+
+
+    return(
+        <div className="container-fluid mt-4">
+            <div className="d-flex justify-content-center align-items-center">
+                <h1 className="text-center text-primary">Register Form</h1>
+            </div>
+        </div>
+    )
+}
